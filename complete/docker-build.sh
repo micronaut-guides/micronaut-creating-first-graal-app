158108a87ecc940a77478e57d140d30289823d2e
Iván López <lopez.ivan@gmail.com>
2019-04-10 16:20:16 +0200

    Update guide to RC15 and rename native image app

--- complete/docker-build.sh
+++ complete/docker-build.sh
@@ -1,5 +1,5 @@
-docker build . -t complete
+docker build . -t micronaut-graal-app
 echo
 echo
 echo "To run the docker container execute:"
-echo "    $ docker run --network host complete"
+echo "    $ docker run --network host micronaut-graal-app"
