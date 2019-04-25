550531b68a73b76a1a32c823fc61f38f065a828a
Iván López <lopez.ivan@gmail.com>
2019-04-25 11:55:38 +0200

    Upgrade to Graal RC16

--- complete/docker-build.sh
+++ complete/docker-build.sh
@@ -2,4 +2,4 @@ docker build . -t micronaut-graal-app
 echo
 echo
 echo "To run the docker container execute:"
-echo "    $ docker run --network host micronaut-graal-app"
+echo "    $ docker run -p 8080:8080 micronaut-graal-app"
