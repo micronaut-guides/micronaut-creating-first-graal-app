205c02c1bde90b83e6bd3af4f661d18e27d76d47
Iván López <lopezi@objectcomputing.com>
2019-11-08 14:26:24 +0100

    Use @MicronautTest

--- complete/src/test/java/example/micronaut/ConferenceControllerTest.java
+++ complete/src/test/java/example/micronaut/ConferenceControllerTest.java
@@ -1,40 +1,26 @@
 package example.micronaut;
 
-import io.micronaut.context.ApplicationContext;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.client.HttpClient;
-import io.micronaut.runtime.server.EmbeddedServer;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.test.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@MicronautTest
 public class ConferenceControllerTest {
 
-    private static EmbeddedServer server;
-    private static HttpClient client;
-
-    @BeforeAll
-    public static void setupServer() {
-        server = ApplicationContext.run(EmbeddedServer.class); // <1>
-        client = server.getApplicationContext().createBean(HttpClient.class, server.getURL()); // <2>
-    }
-
-    @AfterAll
-    public static void stopServer() {
-        if (server != null) {
-            server.stop();
-        }
-        if (client != null) {
-            client.stop();
-        }
-    }
+    @Inject
+    @Client("/")
+    HttpClient client;
 
     @Test
     public void testHello() throws Exception {
         HttpRequest request = HttpRequest.GET("/conferences/random");
-        String body = client.toBlocking().retrieve(request); // <3>
+        String body = client.toBlocking().retrieve(request);
         assertNotNull(body);
     }
 }
